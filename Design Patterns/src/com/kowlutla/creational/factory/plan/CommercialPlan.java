a42e5684fde138ca0181fe05e473241fe6b68b8f
Kowlutla <kowlutla1119@gmail.com>
2022-11-30 11:39:16 +0530

    Factory Pattern using Bill Generation Example

--- Design Patterns/src/com/kowlutla/creational/factory/plan/CommercialPlan.java
+++ Design Patterns/src/com/kowlutla/creational/factory/plan/CommercialPlan.java
@@ -1,10 +1,9 @@
 package com.kowlutla.creational.factory.plan;
 
 public class CommercialPlan extends Plan {
-
 	@Override
 	void getRate() {
 		rate = 7.50;
 	}
-
 }
+
--- Design Patterns/src/com/kowlutla/creational/factory/plan/DomesticPlan.java
+++ Design Patterns/src/com/kowlutla/creational/factory/plan/DomesticPlan.java
@@ -8,3 +8,4 @@ public class DomesticPlan extends Plan {
 	}
 
 }
+
